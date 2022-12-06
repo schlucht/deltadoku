a659d1b5c365059b0939b4b89e89c0467c16fc98
schlucht <web@jagolo.ch>
2022-12-06 15:15:13 +0000

    Load file in HTML

--- frontend/src/components/FhxFileLoad.vue
+++ frontend/src/components/FhxFileLoad.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-    <form>
-        <input @change="laodFile" type="file" ref="file">
-    </form>
+
+        <input @change="loadFile($event)" type="file">
+
   </div>
 </template>
 
-<script>
-    function loadFile() {
-        const formData = new FormData() 
-        console.log(formData)
+<script setup>
+
+
+  const loadFile = (e) => {
+    let file = e.target.files[0]
+    console.log(file)
+    const rf = new FileReader()
+    rf.readAsText(file)
+
+    rf.onload = function() {
+      console.log(rf.result)
     }
+    
+  }
+    
 </script>
 
 <style>
--- web/server.go
+++ web/server.go
@@ -8,7 +8,7 @@ import (
 	"github.com/schlucht/deltadoku/web/routes"
 )
 
-const PORT = ":8080"
+const PORT = ":1234"
 
 func LoadServer() {
 	router := httprouter.New()
